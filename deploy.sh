72966a3985b9079349a73d37117798b0b3a9aaa9
zhaoyifan <993445491@qq.com>
2022-06-18 01:20:24 +0800

    ci: master分支

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhao-yi-fan/bunny-ui.git v2:gh-pages-v2
+git push -f git@github.com:zhao-yi-fan/bunny-ui.git master:gh-pages
 
 cd -
